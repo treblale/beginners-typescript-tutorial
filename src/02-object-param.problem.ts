78265478652ea610cfc4c48c1e2c6b495ac9af55
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-10-25 10:58:47 -0700

    Revert "solved 1-4"
    This reverts commit e9542d359a649619edeeabafa47129cb9a192172.

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,11 +1,6 @@
 import { expect, it } from "vitest";
 
-type paramTypes = {
-  first: number;
-  second: number;
-};
-
-export const addTwoNumbers = (params: paramTypes) => {
+export const addTwoNumbers = (params) => {
   return params.first + params.second;
 };
 
@@ -14,13 +9,13 @@ it("Should add the two numbers together", () => {
     addTwoNumbers({
       first: 2,
       second: 4,
-    })
+    }),
   ).toEqual(6);
 
   expect(
     addTwoNumbers({
       first: 10,
       second: 20,
-    })
+    }),
   ).toEqual(30);
 });
